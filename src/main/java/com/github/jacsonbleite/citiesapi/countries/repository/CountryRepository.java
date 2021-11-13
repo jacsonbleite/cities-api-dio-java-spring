a7b96194bf52b93b47517cb3be5b27f63f3593a9
jacsonbleite <jacsonbleite@gmail.com>
2021-11-12 23:53:56 -0400

    Primeiro commit API Java com Spring

---
+++ src/main/java/com/github/jacsonbleite/citiesapi/countries/repository/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.github.jacsonbleite.citiesapi.countries.repository;
+
+import com.github.jacsonbleite.citiesapi.countries.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+
+
+}
